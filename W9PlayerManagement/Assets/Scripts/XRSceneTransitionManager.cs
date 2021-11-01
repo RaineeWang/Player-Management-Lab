89b36ee9a4f192def093f7057df712f83f8ae039
RaineeWang <77654869+RaineeWang@users.noreply.github.com>
2021-11-01 15:50:30 +0400

    First Video Done
    Socket;
    Global player manager;

--- W9PlayerManagement/Assets/Scripts/XRSceneTransitionManager.cs
+++ W9PlayerManagement/Assets/Scripts/XRSceneTransitionManager.cs
@@ -46,7 +46,7 @@ public class XRSceneTransitionManager : MonoBehaviour
         {
             SceneManager.SetActiveScene(newScene);
             currentScene = newScene;
-            PlaceXRRig(xrScene, currentScene);
+            ConfigureNewXRScene(xrScene, currentScene);
         }
     }
 
@@ -83,18 +83,26 @@ public class XRSceneTransitionManager : MonoBehaviour
             yield return null;
     }
 
-    public static void PlaceXRRig(Scene xrScene, Scene newScene)
+    public static void ConfigureNewXRScene(Scene xrScene, Scene newScene)
     {
         GameObject[] xrObjects = xrScene.GetRootGameObjects();
         GameObject[] newSceneObjects = newScene.GetRootGameObjects();
 
         GameObject xrRig = xrObjects.First((obj) => { return obj.CompareTag("XRRig"); });
-        GameObject xrRigOrigin = newSceneObjects.First((obj) => { return obj.CompareTag("XRRigOrigin"); });
+        GameObject sceneControllerObj = newSceneObjects.First((obj) => { return obj.CompareTag("XRSceneController"); });
 
-        if (xrRig && xrRigOrigin)
+        XRSceneController sceneController = sceneControllerObj.GetComponent<XRSceneController>();
+        if (sceneController)
         {
-            xrRig.transform.position = xrRigOrigin.transform.position;
-            xrRig.transform.rotation = xrRigOrigin.transform.rotation;
+            sceneController.Init();
+
+            Transform xrRigOrigin = sceneController.GetXRRigOrigin();
+
+            if (xrRig && xrRigOrigin)
+            {
+                xrRig.transform.position = xrRigOrigin.transform.position;
+                xrRig.transform.rotation = xrRigOrigin.transform.rotation;
+            }
         }
     }
 
@@ -107,6 +115,11 @@ public class XRSceneTransitionManager : MonoBehaviour
             yield return null;
         }
         transitionMaterial.SetFloat("_FadeAmount", dst);
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this) Instance = null;
     }
 
 }
